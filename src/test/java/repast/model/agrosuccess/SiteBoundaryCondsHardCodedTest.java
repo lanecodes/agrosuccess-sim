4d419b399ac15ce42ad5ce76ce3de220f31406ed
Andrew Lane <ajlane50@gmail.com>
2019-02-27 17:46:35 +0000

    Implement SiteBoundaryCondsHardCoded

---
+++ src/test/java/repast/model/agrosuccess/SiteBoundaryCondsHardCodedTest.java
@@ -0,0 +1,29 @@
+package repast.model.agrosuccess;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SiteBoundaryCondsHardCodedTest {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
