af5f52ccc9cfa2193a2bf0290bc79f5d0b11854e
Andrew Lane <ajlane50@gmail.com>
2019-02-27 18:19:41 +0000

    Implement basic usage tests for SiteBoundaryCondsHardCoded

--- src/test/java/repast/model/agrosuccess/SiteBoundaryCondsHardCodedTest.java
+++ src/test/java/repast/model/agrosuccess/SiteBoundaryCondsHardCodedTest.java
@@ -1,29 +1,56 @@
 package repast.model.agrosuccess;
 
 import static org.junit.Assert.*;
+import java.io.File;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import repast.simphony.valueLayer.GridValueLayer;
 
 public class SiteBoundaryCondsHardCodedTest {
-
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {}
-
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {}
+  
+  File testDataDir = new File("src/test/resources");
+  File initialLandCoverMapFile, soilMapFile, slopeMapFile, aspectMapFile, flowDirMapFile;
 
   @Before
-  public void setUp() throws Exception {}
+  public void setUp() {
+    this.initialLandCoverMapFile = new File(testDataDir, "lct_dummy.tif");
+    this.soilMapFile = new File(testDataDir, "soil_map_dummy.tif");
+    this.slopeMapFile = new File(testDataDir, "slope_dummy.tif");
+    this.aspectMapFile = new File(testDataDir, "aspect_dummy.tif");
+    this.flowDirMapFile = new File(testDataDir, "flow_dir_dummy.tif");
+  }
 
   @After
-  public void tearDown() throws Exception {}
+  public void tearDown() {
+    this.initialLandCoverMapFile = null;
+    this.soilMapFile = null;
+    this.slopeMapFile = null;
+    this.aspectMapFile = null;
+    this.flowDirMapFile = null;
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void shouldRetrieveExpectedValueLayers() {
+    SiteBoundaryConds boundaryConds = new SiteBoundaryCondsHardCoded(50, 25.0, 
+        this.initialLandCoverMapFile, this.soilMapFile, this.slopeMapFile, this.aspectMapFile, 
+        this.flowDirMapFile);
+    
+    assertTrue(boundaryConds.getInitialLandCoverMap() instanceof GridValueLayer);
+    assertTrue(boundaryConds.getSoilMap() instanceof GridValueLayer);
+    assertTrue(boundaryConds.getSlopeMap() instanceof GridValueLayer);
+    assertTrue(boundaryConds.getAspectMap() instanceof GridValueLayer);
+    assertTrue(boundaryConds.getFlowDirMap() instanceof GridValueLayer);
+  }
+  
+  @Test
+  public void shouldRetrieveExpectedNumericalValues() {
+    SiteBoundaryConds boundaryConds = new SiteBoundaryCondsHardCoded(50, 25.0, 
+        this.initialLandCoverMapFile, this.soilMapFile, this.slopeMapFile, this.aspectMapFile, 
+        this.flowDirMapFile);
+    
+    assertEquals(50, boundaryConds.getMeanAnnualPrecipitation());
+    assertEquals(25.0, boundaryConds.getGridPixelSize(), 0.0001); 
   }
 
 }
