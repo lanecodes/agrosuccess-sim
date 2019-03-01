7fae99b5c128792e8424d096601d19af8a360f63
Andrew Lane <ajlane50@gmail.com>
2019-03-01 19:19:59 +0000

    Add code to generate dummy lct and soil maps to bash script

--- src/test/resources/make_geo_test_data.sh
+++ src/test/resources/make_geo_test_data.sh
@@ -19,3 +19,9 @@ write_gtiff hydro_correct_dummy.tif "1 1 3\n3 3 3\n3 4 4"
 
 # write hydrologically incorrect 3x3 demo data (no sink)
 write_gtiff hydro_incorrect_dummy.tif "1 1 5\n3 3 3\n3 4 4"
+
+# Write lct map, mix of pine (6) , oak (9) and burnt(1)
+write_gtiff lct_map_mix_oak_pine_burnt_dummy.tif "6 6 1\n1 1 1\n9 9 9"
+
+# write soil type map, all type A soil
+write_gtiff soil_type_uniform_A_dummy.tif "0 0 0\n0 0 0\n0 0 0"
