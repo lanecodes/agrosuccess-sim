b056d70005c6bea1cc69972e9f5f106ad88977e3
Andrew Lane <ajlane50@gmail.com>
2020-01-24 19:17:15 +0000

    Update misc tests to use new test data location

--- src/test/java/me/ajlane/geo/repast/soilmoisture/SoilMoistureCalculatorTest.java
+++ src/test/java/me/ajlane/geo/repast/soilmoisture/SoilMoistureCalculatorTest.java
@@ -22,25 +22,25 @@ import repast.simphony.space.grid.StrictBorders;
 import repast.simphony.valueLayer.GridValueLayer;
 
 public class SoilMoistureCalculatorTest {
-	
+
 	private static Context<Object> context = new DefaultContext<Object>();
-	private static GridValueLayer flowDirectionGrid;	
+	private static GridValueLayer flowDirectionGrid;
 	private static GridValueLayer soilMoistureLayer;
 	private static SoilMoistureCalculator soilMoistureCalc;
-	
+
 	static void printGridValueLayer(GridValueLayer gvl){
 		for (int y=(int)gvl.getDimensions().getHeight()-1; y>=0; y--) {
-			for (int x=0; x<gvl.getDimensions().getWidth(); x++) {			
+			for (int x=0; x<gvl.getDimensions().getWidth(); x++) {
 				//System.out.format("x: %d, y: %d, value: %.0f\n", x, y, gvl.get(x, y));
 				System.out.format("%.2f\t", (double)Math.round(gvl.get(x, y)*100)/100);
 			}
 			System.out.print("\n");
 		}
-	}	
-	
+	}
+
 	/**
 	 * Set up the test matrix by reading dummy data from file
-	 * 
+	 *
 	 * @throws NotStrictlyPositiveException
 	 * @throws NumberIsTooLargeException
 	 * @throws DataSourceException
@@ -49,47 +49,47 @@ public class SoilMoistureCalculatorTest {
 	@BeforeClass
 	public static void initialiseTestMatrix() throws NotStrictlyPositiveException, NumberIsTooLargeException, DataSourceException, IOException {
 		flowDirectionGrid = (new GeoRasterValueLayer(
-				"src/test/resources/hydro_correct_dummy.tif", 
+				"data/test/hydro_correct_dummy.tif",
 				"flow direction")).getValueLayer();
-		
+
 		System.out.println("flowDirectionGrid:");
 		printGridValueLayer(flowDirectionGrid);
 	}
-	
+
 	/**
 	 * check soil moisture layer matches pen-and-paper check
 	 */
 	@Test
 	public void updatedSoilMoistureValueLayerShouldBeThis() {
 		//add empty soil moisture layer
-		soilMoistureLayer = new GridValueLayer(LscapeLayer.SoilMoisture.name(), 0, true, 
+		soilMoistureLayer = new GridValueLayer(LscapeLayer.SoilMoisture.name(), 0, true,
 		    new StrictBorders(), new int[]{3, 3}, new int[]{0, 0});
 		context.addValueLayer(soilMoistureLayer);
-			
+
 		// add required ValueLayer-s with dummy data to context
 		context.addValueLayer(new DummySlopeLayer3x3(LscapeLayer.Slope.name(), "medium")); // all cells have slope = 5%
 		context.addValueLayer(new DummySoilTypeLayer3x3(LscapeLayer.SoilType.name(), "B")); //  all cells have soil type B
 		context.addValueLayer(new DummyLandCoverTypeLayer3x3(LscapeLayer.Lct.name(), "pine forest")); // all cells occupied by pine forest
-		
+
 		soilMoistureCalc = new SoilMoistureCalculator(flowDirectionGrid, 50.0, context);
-		
+
 		// update soil moisture layer with 50mm of precipitation
 		soilMoistureCalc.updateSoilMoistureLayer(50.0);
-		
+
 		double[][] trueSoilMoistureLayer = {
 				{0+50, 48.0238 + 50, 2*48.0238 + 50 -48.0238},
 				{48.0238 + 50, 50, -48.0238 + 50},
 				{-48.0238 + 50, -48.0238 + 50, -48.0238 + 50}
 		};
-		
+
 		System.out.println("soilMoistureLayer:");
-		printGridValueLayer((GridValueLayer)context.getValueLayer(LscapeLayer.SoilMoisture.name()));		
-		
+		printGridValueLayer((GridValueLayer)context.getValueLayer(LscapeLayer.SoilMoisture.name()));
+
 		for (int i=0; i<soilMoistureLayer.getDimensions().getHeight(); i++) {
 			for (int j=0; j<soilMoistureLayer.getDimensions().getWidth(); j++){
 				assertEquals(trueSoilMoistureLayer[i][j], soilMoistureLayer.get(j, 3-1-i), 0.001);
 			}
 		}
-	}	
-	
+	}
+
 }
