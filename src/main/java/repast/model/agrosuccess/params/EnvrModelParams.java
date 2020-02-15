bd4606a25531e118c1823188c1ee3de4f0a9689a
Andrew Lane <ajlane50@gmail.com>
2020-02-15 14:44:24 +0000

    Parse model parameters using ModelParamsRepastParser

--- src/main/java/repast/model/agrosuccess/params/EnvrModelParams.java
+++ src/main/java/repast/model/agrosuccess/params/EnvrModelParams.java
@@ -9,22 +9,23 @@ import me.ajlane.geo.repast.soilmoisture.SoilMoistureParams;
  *
  */
 public interface EnvrModelParams {
-  
+
   /**
-   * @return Parameters specifying the probability distributions which control how seeds are 
-   *    dispersed in the model.
+   * @return Parameters specifying the probability distributions which control how seeds are
+   *         dispersed in the model.
    */
   public SeedDispersalParams getSeedDispersalParams();
-  
+
   /**
-   * @return Parameters specifying how long different types of seeds survive in the seed bank after 
-   *    being deposited.
+   * @return Parameters specifying how long different types of seeds survive in the seed bank after
+   *         being deposited.
    */
   public SeedViabilityParams getSeedViabilityParams();
-  
+
   /**
-   * @return Parameters needed to determine how to discretise continuous soil moisture values into 
-   *    discrete states. 
+   * @return Parameters needed to determine how to discretise continuous soil moisture values into
+   *         discrete states.
    */
   public SoilMoistureParams getSoilMoistureParams();
+
 }
