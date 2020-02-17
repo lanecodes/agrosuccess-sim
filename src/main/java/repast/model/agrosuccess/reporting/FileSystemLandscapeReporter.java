c8715b82ea207da75f854f39652661813a5e5ca5
Andrew Lane <ajlane50@gmail.com>
2020-02-17 16:42:04 +0000

    Change access permissions in FileSystemLandscapeReporter
    Avoid warnings in eclipse

--- src/main/java/repast/model/agrosuccess/reporting/FileSystemLandscapeReporter.java
+++ src/main/java/repast/model/agrosuccess/reporting/FileSystemLandscapeReporter.java
@@ -5,22 +5,22 @@ import org.apache.commons.lang3.StringUtils;
 import repast.simphony.context.Context;
 import repast.simphony.engine.schedule.ISchedule;
 
-public class FileSystemLandscapeReporter extends LandscapeReporter {  
-  
-  private File outputDir;  
-  private File outputCsvFile;
+public class FileSystemLandscapeReporter extends LandscapeReporter {
 
-  public FileSystemLandscapeReporter(Context<Object> context, ISchedule schedule, String simID, 
+  protected File outputDir;
+  protected File outputCsvFile;
+
+  public FileSystemLandscapeReporter(Context<Object> context, ISchedule schedule, String simID,
       File outputDir, boolean writeLctMap, boolean writeLctProps, int bufferSize) {
     super(context, schedule, simID, writeLctMap, writeLctProps, bufferSize);
     this.outputDir = outputDir;
     this.outputDir.mkdirs(); // ensure outputDir exists
-    this.outputCsvFile = new File(outputDir, simID + ".csv");    
+    this.outputCsvFile = new File(outputDir, simID + ".csv");
   }
-  
-  private void writeLctPropsToCsv() {    
+
+  private void writeLctPropsToCsv() {
   }
-  
+
   @Override
   void reportLctProportions() {
     this.lctPropsQueue.add(this.lctPropAgg.getLctProportions());
@@ -28,20 +28,20 @@ public class FileSystemLandscapeReporter extends LandscapeReporter {
       writeLctPropsToCsv();
     }
   }
-  
-  
+
+
   /**
-   * @param Number of digits to pad the timestep number with. E.g. for a simulation with ID "run10" 
+   * @param Number of digits to pad the timestep number with. E.g. for a simulation with ID "run10"
    *    to make timestep 345 rave ID "run10_00345" set padNum to 5.
-   * @return A unique identifier for the timestep constructed out of both the simulation ID and 
+   * @return A unique identifier for the timestep constructed out of both the simulation ID and
    *    the timestep.
    */
-  private String getTimeStepID(int padNum) {
+  protected String getTimeStepID(int padNum) {
     Integer ts = (int) this.schedule.getTickCount();
     return this.simID + "_" + StringUtils.leftPad(ts.toString(), 5, "0");
   }
-  
-  private String getTimeStepID() {
+
+  protected String getTimeStepID() {
     return getTimeStepID(5);
   }
 
